28114c1af5b6450314150def23d0b1017a051302
mrtrix64 <anneemanuel@Annes-MacBook-Pro.local>
2016-03-14 16:34:04 +0000

    First upload

---
+++ data/delegates.php
@@ -0,0 +1,45 @@
+<?php
+    //Create Database connection
+  /* $db = mysql_connect("internal-db.s99712.gridserver.com","db99712_user","treestump2010");
+    if (!$db) {
+        die('Could not connect to db: ' . mysql_error());
+    }*/
+	
+	
+	
+	
+//LOCAL
+$db = mysql_connect("localhost","db99712_anne","treestump2010");
+    if (!$db) {
+        die('Could not connect to db: ' . mysql_error());
+    }
+
+
+
+    //Select the Database
+    mysql_select_db("db99712_advantage",$db);
+    
+    //Replace * in the query with the column names.
+    $result = mysql_query("SELECT * FROM delegates ORDER BY Organization ASC", $db);  
+	//$result = mysql_query("SELECT * FROM delegates WHERE Privacy_Required = 'F' ORDER BY Organization ASC", $db);
+
+
+header("Content-Type: application/xml");
+ echo "<recordset>"."\n";
+    while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+		echo "<records>"."\n";
+		echo "<delegate_id><![CDATA[".htmlspecialchars($row['delegate_id'], ENT_QUOTES)."]]></delegate_id>"."\n";
+		echo "<First_Name><![CDATA[".($row['First_Name'])."]]></First_Name>"."\n";
+		echo "<Last_Name><![CDATA[".($row['Last_Name'])."]]></Last_Name>"."\n";
+		echo "<Organization><![CDATA[".($row['Organization'])."]]></Organization>"."\n";
+		echo "<Email_Address><![CDATA[".htmlspecialchars($row['Email_Address'], ENT_QUOTES)."]]></Email_Address>"."\n";
+		echo "<Position><![CDATA[".($row['Position'])."]]></Position>"."\n";
+		echo "<Privacy_Required><![CDATA[".htmlspecialchars($row['Privacy_Required'], ENT_QUOTES)."]]></Privacy_Required>"."\n";
+		echo "<G><![CDATA[".htmlspecialchars($row['G'], ENT_QUOTES)."]]></G>"."\n";
+		echo "</records>"."\n";
+    }
+    //echo json_encode($json_response);
+    //Close the database connection
+    fclose($db);
+	echo "</recordset>";
+?>
---
+++ externalConn.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+var dsSponsors = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/sponsors.php?view_xml=1?view_xml=1", "recordset/record", {sortOrderOnLoad: "ascending"});
+dsSponsors.setColumnType("description", "html");
+dsSponsors.setColumnType("aims", "html");
+dsSponsors.setColumnType("website", "html");
+	var dsSchedule = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/schedule.php?view_xml=1", "recordset/record", {sortOnLoad: "sort_order", sortOrderOnLoad: "ascending"});
+dsSchedule.setColumnType("title", "html");
+dsSchedule.setColumnType("sort_order", "number");
+	var dsHotels = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/hotels.php?view_xml=1", "recordset/record", {sortOnLoad: "hotel_name", sortOrderOnLoad: "ascending"});
+dsHotels.setColumnType("hotel_name", "html");
+dsHotels.setColumnType("google_map", "html");
+dsHotels.setColumnType("description", "html");
+dsHotels.setColumnType("amenities", "html");
+dsHotels.setColumnType("website", "html");
+var dsSpecialist = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/specialist.php?view_xml=1", "recordset/record");
+dsSpecialist.setColumnType("cms_content", "html");
+dsSpecialist.setColumnType("sort_order", "number");
+dsSpecialist.setColumnType("spkr_content", "html");
+var dsPlenary = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/plenary.php?view_xml=1", "recordset/record");
+dsPlenary.setColumnType("cms_content", "html");
+dsPlenary.setColumnType("sort_order", "number");
+dsPlenary.setColumnType("spkr_content", "html");
+var dsForum = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/marketing.php?view_xml=1", "recordset/record");
+dsForum.setColumnType("cms_content", "html");
+dsForum.setColumnType("sort_order", "number");
+dsForum.setColumnType("spkr_content", "html");
+var dsNews = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/news.php?view_xml=1", "recordset/record");
+dsNews.setColumnType("news_article", "html");
+var dsSocial = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/social.php?view_xml=1", "recordset/record");
+dsSocial.setColumnType("cms_content", "html");
+dsSocial.setColumnType("sort_order", "number");
+var dsDates = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/dates.php?view_xml=1", "recordset/record", {sortOnLoad: "schedule_title_id", sortOrderOnLoad: "ascending"});
+dsDates.setColumnType("schedule_title_id", "number");
+dsDates.setColumnType("schedule_date", "date");
+
+var delegates = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/delegates2.php?view_xml=1", "recordset/record");
