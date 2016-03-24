3b3733d488a58227241a9df61afcb6a5e29d3208
mrtrix64_Colin <post@colinmiller.co.uk>
2016-03-24 17:56:49 +0000

    Without function

--- internet_connection.js
+++ internet_connection.js
@@ -48,15 +48,15 @@
 
     // Wait for Cordova to load
     // 
-    document.addEventListener("deviceready", onDeviceReady, false);
+    //document.addEventListener("deviceready", onDeviceReady, false);
 
     // Cordova is loaded and it is now safe to make calls Cordova methods
     //
-    function onDeviceReady() {
-        checkConnection();
-    }
+    //function onDeviceReady() {
+        //checkConnection();
+    //}
 
-    function checkConnection() {
+    //function checkConnection() {
         var networkState = navigator.network.connection.type;
 		//alert(networkState);
 
@@ -79,41 +79,41 @@
 			
 		 else 	{
 			 alert ("Connected");
-			 var dsSponsors = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/sponsors.php?view_xml=1", "recordset/record", {sortOrderOnLoad: "ascending"});
-dsSponsors.setColumnType("description", "html");
-dsSponsors.setColumnType("aims", "html");
-dsSponsors.setColumnType("website", "html");
-var dsSchedule = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/schedule.php?view_xml=1", "recordset/record", {sortOnLoad: "sort_order", sortOrderOnLoad: "ascending"});
-dsSchedule.setColumnType("title_date", "number");
-dsSchedule.setColumnType("title", "html");
-dsSchedule.setColumnType("sort_order", "number");
-var dsLeisure = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/leisure.php?view_xml=1", "recordset/record");
-dsLeisure.setColumnType("cms_content", "html");
-dsLeisure.setColumnType("sort_order", "number");
-dsLeisure.setColumnType("speaker1_desc", "html");
-dsLeisure.setColumnType("speaker2_desc", "html");
-dsLeisure.setColumnType("speaker3_desc", "html");
-dsLeisure.setColumnType("date_time", "number");
-var dsPlenary = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/plenary.php?view_xml=1", "recordset/record");
-dsPlenary.setColumnType("cms_content", "html");
-dsPlenary.setColumnType("sort_order", "number");
-dsPlenary.setColumnType("speaker1_desc", "html");
-dsPlenary.setColumnType("speaker2_desc", "html");
-dsPlenary.setColumnType("speaker3_desc", "html");
-dsPlenary.setColumnType("date_time", "number");
-var dsBreakout = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/breakout.php?view_xml=1", "recordset/record");
-dsBreakout.setColumnType("cms_content", "html");
-dsBreakout.setColumnType("sort_order", "number");
-dsBreakout.setColumnType("speaker1_desc", "html");
-dsBreakout.setColumnType("speaker2_desc", "html");
-dsBreakout.setColumnType("speaker3_desc", "html");
-dsBreakout.setColumnType("date_time", "number");
-var dsNews = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/news.php?view_xml=1", "recordset/record");
-dsNews.setColumnType("news_article", "html");
-var dsSocial = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/social.php?view_xml=1", "recordset/record");
-dsSocial.setColumnType("cms_content", "html");
-dsSocial.setColumnType("sort_order", "number");
-dsSocial.setColumnType("date_time", "number");
-var delegates = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/delegates2.php?view_xml=1", "recordset/record");
+			 	var dsSponsors = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/sponsors.php?view_xml=1", "recordset/record", {sortOrderOnLoad: "ascending"});
+				dsSponsors.setColumnType("description", "html");
+				dsSponsors.setColumnType("aims", "html");
+				dsSponsors.setColumnType("website", "html");
+				var dsSchedule = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/schedule.php?view_xml=1", "recordset/record", {sortOnLoad: "sort_order", sortOrderOnLoad: "ascending"});
+				dsSchedule.setColumnType("title_date", "number");
+				dsSchedule.setColumnType("title", "html");
+				dsSchedule.setColumnType("sort_order", "number");
+				var dsLeisure = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/leisure.php?view_xml=1", "recordset/record");
+				dsLeisure.setColumnType("cms_content", "html");
+				dsLeisure.setColumnType("sort_order", "number");
+				dsLeisure.setColumnType("speaker1_desc", "html");
+				dsLeisure.setColumnType("speaker2_desc", "html");
+				dsLeisure.setColumnType("speaker3_desc", "html");
+				dsLeisure.setColumnType("date_time", "number");
+				var dsPlenary = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/plenary.php?view_xml=1", "recordset/record");
+				dsPlenary.setColumnType("cms_content", "html");
+				dsPlenary.setColumnType("sort_order", "number");
+				dsPlenary.setColumnType("speaker1_desc", "html");
+				dsPlenary.setColumnType("speaker2_desc", "html");
+				dsPlenary.setColumnType("speaker3_desc", "html");
+				dsPlenary.setColumnType("date_time", "number");
+				var dsBreakout = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/breakout.php?view_xml=1", "recordset/record");
+				dsBreakout.setColumnType("cms_content", "html");
+				dsBreakout.setColumnType("sort_order", "number");
+				dsBreakout.setColumnType("speaker1_desc", "html");
+				dsBreakout.setColumnType("speaker2_desc", "html");
+				dsBreakout.setColumnType("speaker3_desc", "html");
+				dsBreakout.setColumnType("date_time", "number");
+				var dsNews = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/news.php?view_xml=1", "recordset/record");
+				dsNews.setColumnType("news_article", "html");
+				var dsSocial = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/social.php?view_xml=1", "recordset/record");
+				dsSocial.setColumnType("cms_content", "html");
+				dsSocial.setColumnType("sort_order", "number");
+				dsSocial.setColumnType("date_time", "number");
+				var delegates = new Spry.Data.XMLDataSet("http://advantagetravel.e-update.co.uk/mobile/data/delegates2.php?view_xml=1", "recordset/record");
     }
-	}
+	//}
